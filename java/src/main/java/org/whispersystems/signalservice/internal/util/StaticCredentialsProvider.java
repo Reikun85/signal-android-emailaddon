e00434bed5648335c51ec360f6239bcca4ad7780
Andrea Taglieri <andrea@mbp-di-andrea.fabaris.lan>
2019-04-09 14:39:38 +0200

    init

---
+++ java/src/main/java/org/whispersystems/signalservice/internal/util/StaticCredentialsProvider.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2014-2016 Open Whisper Systems
+ *
+ * Licensed according to the LICENSE file in this repository.
+ */
+
+package org.whispersystems.signalservice.internal.util;
+
+import org.whispersystems.signalservice.api.util.CredentialsProvider;
+
+public class StaticCredentialsProvider implements CredentialsProvider {
+
+  private final String user;
+  private final String email;
+  private final String password;
+  private final String signalingKey;
+
+  public StaticCredentialsProvider(String user, String password, String email, String signalingKey) {
+    this.user         = user;
+    this.email        = email;
+    this.password     = password;
+    this.signalingKey = signalingKey;
+  }
+
+  @Override
+  public String getUser() {
+    return user;
+  }
+
+  @Override
+  public String getEmail() {
+    return email;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getSignalingKey() {
+    return signalingKey;
+  }
+}
